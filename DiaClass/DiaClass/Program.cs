509cf77aa07cc6a478b472ae5bcd67c239443cfa
Alessio Lusa <alusa@dca.it>
2025-09-26 16:20:17 +0200

    adding service class

---
+++ DiaClass/DiaClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace DiaClass;
+
+public class Program
+{
+    public static async Task Main(string[] args) // Define Main as async
+    {
+        Console.WriteLine("# ----------------------------- DiaClass ----------------------------- #");
+        Console.WriteLine("# ------------------ A tool to visualize C# projects ----------------- #");
+        //Console.WriteLine("# --------------------------- Author: Lusi --------------------------- #");
+        Console.WriteLine("# -------------------------------------------------------------------- #");
+
+        Console.WriteLine("Insert here the path to your .sln folder:\n\n");
+
+        string path;
+
+        while (true)
+        {
+            path = Console.ReadLine();
+            if (Loader.CheckPath(path))
+            {
+                Console.WriteLine("\nPath is valid!\n");
+                break;
+            }
+            else
+                Console.WriteLine("\nPath is not valid, please reinsert it:\n");
+        }
+
+        Service service = new Service(path);
+
+        await service.Initialize(); // Await the asynchronous method
+    }
+}
