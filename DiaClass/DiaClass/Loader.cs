cf0aa83e34af17cbb8d6fbe226732d05cc255578
Alessio Lusa <alusa@dca.it>
2025-09-26 16:20:09 +0200

    adding loader class

---
+++ DiaClass/DiaClass/Loader.cs
@@ -0,0 +1,13 @@
+﻿namespace DiaClass;
+
+public static class Loader
+{
+    public static bool CheckPath(string solutionFolderPath)
+    {
+        if (solutionFolderPath == null || solutionFolderPath == string.Empty)
+            return false;
+        return true;
+    }
+
+
+}
